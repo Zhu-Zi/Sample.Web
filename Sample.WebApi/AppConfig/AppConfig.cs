942882f5ce1423957e9d3bc5536a21540daf88ba
Zhu-Zi <513444173@qq.com>
2021-01-12 15:03:45 +0800

    添加配置文件读取

---
+++ Sample.WebApi/AppConfig/AppConfig.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace Sample.WebApi.AppConfig
+{
+    public class AppConfig
+    {
+        private static IConfigurationRoot _config { get; set; }
+
+        static AppConfig()
+        {
+            // Microsoft.Extensions.Configuration 扩展包提供的
+            var evnName = Environment.GetEnvironmentVariable("INSIGHT_ENV");
+            // 测试用
+            //var evnName = "DEV";
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.{evnName}.json", optional: false)
+                .AddEnvironmentVariables();
+            _config = builder.Build();
+        }
+
+        public static IConfigurationRoot AppSettings
+        {
+            get
+            {
+                return _config;
+            }
+        }
+
+        public static string Get(string key)
+        {
+            return _config[key];
+        }
+
+        public static string GetConnectionString(string key)
+        {
+            return _config.GetConnectionString(key);
+        }
+    }
+}
--- Sample.WebApi/Controllers/ValuesController.cs
+++ Sample.WebApi/Controllers/ValuesController.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Sample.WebApi.Models.Request;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Sample.WebApi.Controllers
 {
